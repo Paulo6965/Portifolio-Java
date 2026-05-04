43805bd6daecf1fdfc2b4f0135dd035d0108a7eb
Paulo Henrique <henrique.paulo.ph69@gmail.com>
2026-05-04 10:45:33 -0300

    Principal.java
    Principal incluindo o ar condicionado para testes

--- Principal.java
+++ Principal.java
@@ -3,8 +3,15 @@
 public class Principal {
     public static void main(String[] args) {
         Lampada Mercurio = new Lampada("Lampada de Mercurio");
-    Mercurio.alteraBrilho(25);
-        System.out.println("Atualmente o brilho esta em " +Mercurio.getBrilho() +"%");
+        Mercurio.alteraBrilho(25);
+        System.out.println("\nAtualmente o brilho esta em " +Mercurio.getBrilho() +"%");
         Mercurio.alternarEstado();
-    Mercurio.alteraBrilho(50);}
+        Mercurio.alteraBrilho(50);
+        
+        ArCondicionado Arcon = new ArCondicionado("CongelaFrango");
+        System.out.println("\nAtualmente o Ar condicionado esta " +Arcon.getEstado());
+        Arcon.alternarEstado();
+        Arcon.alteraTemp(25);
+        System.out.println("Atualmente o Ar condicionado esta " +Arcon.getEstado());
+    }
 }
