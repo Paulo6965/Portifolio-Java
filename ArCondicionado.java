dd08819769eac802e976bb7987ed35d7d5f76f85
Paulo Henrique <henrique.paulo.ph69@gmail.com>
2026-05-04 10:44:45 -0300

    ArCondicionado.java
    Praticamente completo

--- ArCondicionado.java
+++ ArCondicionado.java
@@ -1,5 +1,20 @@
 //package SmartHomeProject;
 
-public class ArCondicionado {
-    
+public class ArCondicionado extends Dispositivo {
+    int temp = 22;
+
+    public ArCondicionado (String nome){
+        super(nome);
+    }
+    public void alteraTemp(int valor){
+        if (estado == "Desligado"){
+            System.out.println("Voce so pode alterar a temperatura se o aparelho estiver ligado");
+        }else {
+        temp = valor;
+        System.out.println("A temperatura esta em " +temp+ "°C");
+        }
+    }
+    public int getTemp(){
+        return this.temp;
+    }
 }
