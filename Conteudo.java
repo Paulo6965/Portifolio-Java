9265ced0edfd8049828ae580f1c1abe0223ec305
Paulo Henrique <henrique.paulo.ph69@gmail.com>
2026-05-07 09:37:27 -0300

    Add files via upload

---
+++ Conteudo.java
@@ -0,0 +1,13 @@
+public class Conteudo{
+    protected String nome;
+    protected int duracao;
+    protected String tipo;
+
+    public Conteudo(String nome){
+        this.nome = nome;
+    }
+
+    public Conteudo(int duracao){
+        this.duracao = duracao;
+    }
+}
